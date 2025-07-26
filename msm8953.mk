4f1241342975460674d4ed44380e1002cfd3054e
answer35 <rbohanne@proton.me>
2025-07-26 13:48:41 +0200

    msm8953-common: remove non-existent modules

--- msm8953.mk
+++ msm8953.mk
@@ -51,8 +51,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.print.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.print.xml
 
 # ANT
-PRODUCT_PACKAGES += \
-    AntHalService
+#PRODUCT_PACKAGES += \
+#    AntHalService
 
 # Audio
 PRODUCT_PACKAGES += \
